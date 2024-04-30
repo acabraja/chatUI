b8f4f4c4984e74ff830c4394c4139ea5c47abf4d
draynk <160605811+draynk@users.noreply.github.com>
2024-04-30 17:42:32 +0000

    Changes to be committed:

--- src/features/full/components/Full.tsx
+++ src/features/full/components/Full.tsx
@@ -34,7 +34,7 @@ export const Full = (props: FullProps, { element }: { element: HTMLElement }) =>
         <div
           style={{
             'background-color': props.theme?.chatWindow?.backgroundColor || '#ffffff',
-            height: props.theme?.chatWindow?.height ? `${props.theme?.chatWindow?.height.toString()}px` : '100%',
+            height: props.theme?.chatWindow?.height ? `${props.theme?.chatWindow?.height.toString()}px` : '200px',
             width: props.theme?.chatWindow?.width ? `${props.theme?.chatWindow?.width.toString()}px` : '100%',
             margin: '0px',
           }}
